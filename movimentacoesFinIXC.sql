ef6af1b3ecf4301ee2dbb13ef5767614e8bd1009
Weslley Carvalho <109834027+Wesll-Car02@users.noreply.github.com>
2024-05-22 11:28:40 -0300

    Update movimentacoesFinIXC.sql

--- movimentacoesFinIXC.sql
+++ movimentacoesFinIXC.sql
@@ -11,29 +11,41 @@ WITH Recebimentos AS (
 ),
 resumoGeral AS (
     SELECT
-        m.id,
-        m.data,
+        m.id idMov,
+        m.data Data,
         m.debito AS Entrada,
         m.credito AS Saida,
-        m.id_movim_finan,
-        m.tipo_lanc,
-        COALESCE(r.tipo_recebimento, m.tipo_recebimento) AS tipo_recebimento_ajustado
+        m.id_movim_finan idMov_M,
+        COALESCE(r.tipo_recebimento, m.tipo_recebimento) AS tpRec
     FROM
         fn_movim_finan m
     LEFT JOIN
         Recebimentos r ON m.id_movim_finan = r.id_movim_finan
     WHERE 
-        m.data >= '2024-05-14' AND m.data <= '2024-05-15'
-        AND m.id_conta = '897727'
+        m.data >= '00-05-14' AND m.data <= '2024-04-30'
+        AND m.id_conta = '913204'
     GROUP BY
         m.id,
         m.data,
         m.credito,
         m.debito,
         m.id_movim_finan,
-        m.tipo_lanc,
         COALESCE(r.tipo_recebimento, m.tipo_recebimento)
     ORDER BY
         m.id_movim_finan
-)
-SELECT SUM(Entrada) AS Total_Entrada, SUM(Saida) AS Total_Saida FROM resumoGeral;
+)SELECT * FROM `resumoGeral`;
+/*SELECT 
+    Data,
+    SUM(Entrada) Entrada, 
+    SUM(Saida) Saida, 
+    SUM(Entrada - Saida) Saldo,
+    SUM(SUM(Entrada - Saida)) OVER (ORDER BY Data ASC) AS SaldoAcumulado
+/* A operação acimaresponsável por calcular o saldo acumulado. O SUM(Entrada - Saida) calcula o saldo do dia, 
+e a função de janela OVER (ORDER BY Data ASC) garante que o saldo acumulado seja calculado na ordem dAS datAS
+FROM 
+    resumoGeral
+GROUP BY
+    Data
+ORDER BY 
+    Data ASC
+*/
