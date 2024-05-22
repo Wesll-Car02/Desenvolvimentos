9cf78a5bc9fc8ef6d6ae9b184e42c1136c82d827
Weslley Carvalho <109834027+Wesll-Car02@users.noreply.github.com>
2024-05-22 10:48:35 -0300

    Update movimentacoesFinIXC.sql

--- movimentacoesFinIXC.sql
+++ movimentacoesFinIXC.sql
@@ -8,29 +8,32 @@ WITH Recebimentos AS (
         tipo_lanc != 'M'
     GROUP BY
         id_movim_finan
+),
+resumoGeral AS (
+    SELECT
+        m.id,
+        m.data,
+        m.debito AS Entrada,
+        m.credito AS Saida,
+        m.id_movim_finan,
+        m.tipo_lanc,
+        COALESCE(r.tipo_recebimento, m.tipo_recebimento) AS tipo_recebimento_ajustado
+    FROM
+        fn_movim_finan m
+    LEFT JOIN
+        Recebimentos r ON m.id_movim_finan = r.id_movim_finan
+    WHERE 
+        m.data >= '2024-05-14' AND m.data <= '2024-05-15'
+        AND m.id_conta = '897727'
+    GROUP BY
+        m.id,
+        m.data,
+        m.credito,
+        m.debito,
+        m.id_movim_finan,
+        m.tipo_lanc,
+        COALESCE(r.tipo_recebimento, m.tipo_recebimento)
+    ORDER BY
+        m.id_movim_finan
 )
-SELECT
-    m.id,
-    m.data,
-    m.debito AS Entrada,
-    m.credito AS Saida,
-    m.id_movim_finan,
-    m.tipo_lanc,
-    COALESCE(r.tipo_recebimento, m.tipo_recebimento) AS tipo_recebimento_ajustado
-FROM
-    fn_movim_finan m
-LEFT JOIN
-    Recebimentos r ON m.id_movim_finan = r.id_movim_finan
-WHERE 
-    m.data >= '2024-05-14' AND m.data <= '2024-05-15'
-    AND m.id_conta = '897727'
-GROUP BY
-    m.id,
-    m.data,
-    m.credito,
-    m.debito,
-    m.id_movim_finan,
-    m.tipo_lanc,
-    COALESCE(r.tipo_recebimento, m.tipo_recebimento)
-ORDER BY
-    m.id_movim_finan;
+SELECT SUM(Entrada) AS Total_Entrada, SUM(Saida) AS Total_Saida FROM resumoGeral;
